3bb4101102f7aa359bc720e56604944cfa4183c8
Muhammad Arham <arhamtanwer91@gmail.com>
2025-12-04 14:27:47 +0500

    weather

--- script.mjs
+++ script.mjs
@@ -1,23 +1,27 @@
-let temprature = prompt("Enter the Temp of your Area")
+function Calculate() {
+    let temprature = document.getElementById("temprature").value
 
-if (temprature >= 50){
-    alert("You might have a stroke")
-}
-else if (temprature < 50 && temprature >= 40){
-    alert("It's very hot out there")
-}
-else if (temprature < 40 && temprature >=30){
-    alert("It's hot you should definitely take a water bottle with you")
-}
-else if (temprature < 30 && temprature >=20){
-    alert("Normal Temprature you can probably go out and chill")
-}
-else if (temprature < 20 && temprature >= 10){
-    alert("Jeez it's cold out there")
-}
-else if (temprature < 10){
-    alert("Man!! It's freezing don't go out there")
-}
-else{
-    alert("invalid Input, Please enter Temprature")
+    result = ""
+
+    if (temprature > 50) {
+        result = "You might have a stroke"
+    }
+    else if (temprature >= 40) {
+        result = "Its very Hot out there"
+    }
+    else if (temprature >= 30){
+        result="its a little bit hot but take a water bottle with you"
+    }
+    else if(temprature >=20){
+        result="Normal temprature, you can got out and chill"
+    }
+    else if (temprature >= 10){
+        result="Jeez! its cold out there"
+    }
+    else if(temprature < 10){
+        result="Mann!! its freezing"
+    }
+
+
+    document.getElementById("output").innerText = result
 }
